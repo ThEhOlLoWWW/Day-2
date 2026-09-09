0712fc9a6ea734c9c773cd61716f0bcd2851c2d9
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-09 14:48:27 +0100

    ex3 done

--- day02/exercices/exercice-03.js
+++ day02/exercices/exercice-03.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let a = 7;
+for (let i = 0; i <= 10; i++){
+    console.log(a + " * " + i + " = " + a * i);
+}
